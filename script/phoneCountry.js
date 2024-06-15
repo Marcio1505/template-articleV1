bf93e674e3ccd4f445c28a2dcd2981b5ac78579a
Marcio Jr <jmbtj.mb.15@gmail.com>
2024-06-15 19:41:33 -0300

    correção de bugs v-- stable

--- script/phoneCountry.js
+++ script/phoneCountry.js
@@ -1,19 +1,25 @@
-const url = "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js";
-function getIp(callback) {
-    fetch('https://ipinfo.io/json?token=df26bb0f8ce15c', { headers: { 'Accept': 'application/json' }})
-        .then((resp) => resp.json())
-        .catch(() => {
-            return { country: 'us' };
-        })
-        .then((resp) => callback(resp.country));
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const url = "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js";
+    function getIp(callback) {
+        fetch('https://ipinfo.io/json?token=df26bb0f8ce15c', { headers: { 'Accept': 'application/json' } })
+            .then((resp) => resp.json())
+            .catch(() => {
+                return { country: 'us' };
+            })
+            .then((resp) => callback(resp.country));
+    }
 
-window.onload = function() {
-    const phoneInputField = document.querySelector("#phone");
 
-    const phoneInput = window.intlTelInput(phoneInputField, {
+    var phoneInputField = document.querySelector("input[name='phone']");
+    var countryListContainer = document.getElementById("country-list-container") ||
+        document.getElementsByClassName("iti__flag-container");
+    var iti;
+
+    iti = window.intlTelInput(phoneInputField, {
         initialCountry: "auto",
+        dropdownContainer: countryListContainer,
         geoIpLookup: getIp,
         utilsScript: url,
     });
-};
+})
+
